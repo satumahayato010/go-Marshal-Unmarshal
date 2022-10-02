31fd5a1cb4f42c9ef88b3d7815202de951b22b02
stm33 <satuma33@icloud.com>
2022-10-02 14:47:18 +0900

    [add]refs #9 Unmarshal-customize

--- main.go
+++ main.go
@@ -7,17 +7,30 @@ import (
 
 type Book struct {
 	Title     string `json:"title"`
-	Author    string `json:"-"`
+	Author    string `json:"author"`
 	Publisher string `json:"publisher"`
 }
 
-func (b Book) MarshalJSON() ([]byte, error) {
-	v, err := json.Marshal(&struct {
-		Publisher string
-	}{
-		Publisher: b.Publisher + " Japan",
-	})
-	return v, err
+//func (b Book) MarshalJSON() ([]byte, error) {
+//	v, err := json.Marshal(&struct {
+//		Publisher string
+//	}{
+//		Publisher: b.Publisher + " Japan",
+//	})
+//	return v, err
+//}
+
+func (b *Book) UnmarshalJSON(byte []byte) error {
+	type Book2 struct {
+		Title string
+	}
+	var b2 Book2
+	err := json.Unmarshal(byte, &b2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b.Title = b2.Title + "-より良いコードを書くためのシンプルで実践的なテクニック"
+	return err
 }
 
 func main() {
